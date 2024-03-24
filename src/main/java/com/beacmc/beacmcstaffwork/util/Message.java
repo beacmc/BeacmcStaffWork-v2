513e248f7da804ee3bcfd64b036148a0163057f9
beacmc <kornieiko09@gmail.com>
2024-03-24 15:33:07 +0200

    add Message.java
    Signed-off-by: beacmc <kornieiko09@gmail.com>

---
+++ src/main/java/com/beacmc/beacmcstaffwork/util/Message.java
@@ -0,0 +1,17 @@
+package com.beacmc.beacmcstaffwork.util;
+
+import com.beacmc.beacmcstaffwork.manager.Color;
+import com.beacmc.beacmcstaffwork.manager.configuration.Config;
+
+public class Message {
+
+    public static String fromConfig(String path) {
+        return Color.compile(Config.getString(path)
+                .replace("{PREFIX}", Config.getString("settings.prefix")));
+    }
+
+    public static String of(String message) {
+        return Color.compile(message
+                .replace("{PREFIX}", Config.getString("settings.prefix")));
+    }
+}
